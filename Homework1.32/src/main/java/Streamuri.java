de22deda9cca8c1de4c07ee9706e733030ac15cc
EcaterinaMihaela <capatina.ecaterina.n2y@student.ucv.ro>
2025-03-24 09:40:40 +0200

    Update Streamuri.java

--- Homework1.32/src/main/java/Streamuri.java
+++ Homework1.32/src/main/java/Streamuri.java
@@ -1,21 +1,28 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.List;
 
 public class Streamuri
 {
     public static void main(String[] args) {
-        List<String> lista = Arrays.asList("ana", "maia", "eana", "marcel");
+        List<String> lista = Arrays.asList("ana", "maia", "ionela", "marcela","andreea");
 
-        List<String> rezultat = lista.stream()
+        List<String> rezultat1 = lista.stream()
                 .filter(s -> s.startsWith("a"))  //filtrare
                 .map(String::toUpperCase)  //mapare la uppercase
                 .sorted()  //sortare crescatoare
                 .collect(Collectors.toList());
 
-        System.out.println("Rezultat: " + rezultat);
+        System.out.println("Rezultat: " + rezultat1);
 
+        List<String> rezultat2 = lista.stream()
+                .filter(s -> s.startsWith("a"))  //filtrare
+                .map(String::toUpperCase)  //mapare la uppercase
+                .sorted(Comparator.reverseOrder()).toList();  //sortare descrescatoare
+
+        System.out.println("Rezultat: " + rezultat2);
     }
 }
